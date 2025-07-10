7c77931db27f1bdad755b6243136b6bddf95699b
bobluci <yolfcf2006@gmail.com>
2025-07-09 22:48:46 -0500

    Update ingredientesveganos.py
    se agregó un análisis con gráfica sobre: recetas con más likes, recetsa con menos tiempo de elaboración y Gráfico comparativo de likes vs tiempo

--- analisis/ingredientesveganos.py
+++ analisis/ingredientesveganos.py
@@ -46,3 +46,62 @@ df = pd.DataFrame([{
     "diets": r.get("diets")
 } for r in recetas])
 
+#MÁS LIKES------------------------------------------------------------------------------
+
+top_likes = df.sort_values(by="likes", ascending=False).head(10)
+print(top_likes[["title", "likes"]])
+
+#GRAFICO
+top_likes = df.sort_values(by="likes", ascending=False).head(10)
+plt.figure(figsize=(10, 6))
+plt.barh(top_likes["title"], top_likes["likes"], color="salmon")
+plt.title("Top 10 recetas veganas más populares")
+plt.xlabel("Número de likes")
+plt.gca().invert_yaxis()
+plt.tight_layout()
+plt.savefig("assets/image/ranking_popularidad.png")#GRAFICA GUARDADA EN ASSETS/IMAGE
+plt.close()
+
+
+#TIEMPO DE PREPARACIÓN--------------------------------------------------------------------
+
+top_rapidas = df.sort_values(by="readyInMinutes").reset_index(drop=True)
+
+print("⏳ Recetas veganas más rápidas:")
+for i, row in top_rapidas.head(10).iterrows():
+    print(f"{i+1}. {row['title']} – {row['readyInMinutes']} min")
+
+#GRAFICO
+
+top_rapidas = df.sort_values(by="readyInMinutes").head(10)
+plt.figure(figsize=(10, 6))
+plt.barh(top_rapidas["title"], top_rapidas["readyInMinutes"], color="mediumseagreen")
+plt.title("Top 10 recetas veganas más rápidas")
+plt.xlabel("Minutos de preparación")
+plt.gca().invert_yaxis()
+plt.tight_layout()
+plt.savefig("assets/image/ranking_rapidez.png")#GRAFICA GUARDADA EN ASSETS/IMAGE
+plt.close()
+
+
+#Recetas con mejor puntaje saludable y rápida---------------------------------------------------------------
+
+df["eficiencia_salud"] = df["healthScore"] / df["readyInMinutes"]
+eficientes = df.sort_values(by="eficiencia_salud", ascending=False).head(10)
+
+print("Recetas más eficientes en salud por minuto:")
+for i, r in eficientes.iterrows():
+    print(f"{r['title']} – {round(r['eficiencia_salud'], 2)} pts/min")
+
+#GRAFICO
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data=df, x="readyInMinutes", y="likes", hue="healthScore", palette="viridis")
+plt.title("Popularidad vs Tiempo de Preparación")
+plt.xlabel("Minutos")
+plt.ylabel("Likes")
+plt.tight_layout()
+plt.savefig("assets/image/popularidad_vs_tiempo.png")#GRAFICA GUARDADA EN ASSETS/IMAGE
+plt.close()
